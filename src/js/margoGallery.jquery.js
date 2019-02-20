cc1b9960feab914604a45a30fa63fa18d90c9be4
Hubert Marzec <marzec@8powers.pl>
2019-02-20 21:32:05 +0100

    wip

--- src/js/margoGallery.jquery.js
+++ src/js/margoGallery.jquery.js
@@ -1,13 +1,36 @@
 (function($) {
 
     $.fn.margoGallery = function(options) {
-        var settings = $.extend({
-            itemCss: "gallery__image"
+        const settings = $.extend({
+            itemCss: "gallery__image",
+            container: $('body'),
+            backdropClass: 'backdrop'
         }, options);
         
-        this.each(function(){
-          console.log('$(this)', $(this), settings.itemCss);
-          $(this).addClass(settings.itemCss);
+        const backdropOpenClass = `${settings.backdropClass}--open`;
+
+        const $backdrop = $("<div>abc</div>")
+            .addClass(settings.backdropClass)
+            .appendTo(settings.container)
+            .on('click', function() {
+                $(this).removeClass(backdropOpenClass);
+            })
+
+
+        const showImage = function($el) {
+            const $img = $('<img/>').attr('src', $el.attr('data-url') || $el.attr('src'));
+            $backdrop
+                .html($img)
+                .addClass(backdropOpenClass);
+
+        }
+
+        this.each(function() {
+            $(this)
+                .addClass(settings.itemCss)
+                .on('click', function() {
+                    showImage($(this));
+                });
         });
 
         return this;
