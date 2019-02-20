8f9966eb9693deeee7f4e731ab4ca220d8a18809
Hubert Marzec <marzec@8powers.pl>
2019-02-20 21:04:39 +0100

    wip

--- src/js/margoGallery.jquery.js
+++ src/js/margoGallery.jquery.js
@@ -1,7 +1,14 @@
 (function($) {
 
-    $.fn.margoGallery = function() {
+    $.fn.margoGallery = function(options) {
+        var settings = $.extend({
+            itemCss: "gallery__image"
+        }, options);
         
+        this.each(function(){
+          console.log('$(this)', $(this), settings.itemCss);
+          $(this).addClass(settings.itemCss);
+        });
 
         return this;
     };
