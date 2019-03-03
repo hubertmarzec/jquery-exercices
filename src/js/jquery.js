9f8d5e2e566f7d44ddff0947d540dadd3b880e8a
“[Tomasz <[tomkowalczyk@hotmail.com]>
2019-03-03 21:16:40 +0100

    Add plugin, slide still not working :-( .

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,12 +1,45 @@
 $(document).ready(function () {
 
 
-    $(".accordion__summary").on("click",
-        function () {
-            $("p").hide();
-            $(this).next().show("slow");
-            //$(this).next().slideToggle();
+    /*     $(".accordion__summary").on("click",
+            function () {
+                $("p").hide();
+                //$(this).next().show("slow");
+                $(this).next().toggle();
+    
+            });
+    
+     */    // Does't work with slideDown!!!
+    /*(function ($) {
+        $.fn.slide = function (options) {
 
-        });
+            //$("p").hide();
+
+            var defaults = {
+                duration: 400;
+                easing: swing;
+            };
+
+            var settings = $.extend({}, defaults, options);
+
+            return $(this).next().slideToggle(options);
+        }
+    });
+
+    $(".accordion__summary").on("click", slide, function () {
+        $(this).css("border: 1px solid red;");
+    });
+    */
+
+    $.fn.pSlide = function () {
+
+        $(this).on("click",
+            function () {
+                $("p").hide();
+                //$(this).next().show("slow");
+                $(this).next().toggle();
+
+            });
+    };
+    $(".accordion__summary").pSlide();
 });
-// Does't work with slideDown!!!
